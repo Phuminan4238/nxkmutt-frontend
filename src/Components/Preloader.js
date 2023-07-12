10075420ec88219a97350d6005918b03bb075e61
Phuminan4238 <phuminan@live.com>
2023-07-12 14:06:04 +0700

    update : 12/07/2023 (14:06)

--- src/Components/Preloader.js
+++ src/Components/Preloader.js
@@ -5,13 +5,21 @@ import "./Preloader.css";
 import { preLoaderAnim } from "../Animations";
 import logored from "../Images/logored.svg";
 import logobrain from "../Images/logobrain.svg";
+import logotext from "../Images/logotext.svg";
 
 const Preloader = () => {
   useEffect(() => {
     preLoaderAnim();
   }, []);
 
-  const logoStyle = {
+  const logotextStyle = {
+    width: "100%",
+    height: "auto",
+    // marginLeft: "auto",
+    // marginRight: "4rem",
+  };
+
+  const logobrainStyle = {
     width: "60%",
     height: "auto",
     // marginLeft: "auto",
@@ -22,14 +30,17 @@ const Preloader = () => {
     <div className="preloader">
       <div className="texts-container">
         <span className="w-50 items-center">
-          <img src={logobrain} loading="lazy" style={logoStyle} />
+          <img src={logobrain} loading="lazy" style={logobrainStyle} />
         </span>
-        <span className="pe-4 align-middle">
+        <span className="w-100 items-center">
+          <img src={logotext} loading="lazy" style={logotextStyle} />
+        </span>
+        {/* <span className="pe-4 align-middle">
           Neuroscience Center for Research and Innovation
-        </span>
+        </span> */}
         {/* <span className="pe-3">for Research and Innovation</span> */}
         {/* <span className="pe-4">for Research and Innovation</span> */}
-        <span className="ps-2 align-middle">KMUTT</span>
+        <span className="ps-5 align-middle">KMUTT</span>
       </div>
     </div>
   );
