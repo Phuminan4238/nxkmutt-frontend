68ca48b586fbe970a4ade3a14530a2f360a78784
Phuminan4238 <phuminan@live.com>
2023-07-12 14:41:05 +0700

    update : 12/07/2023 (14:41)

--- src/Components/Preloader.js
+++ src/Components/Preloader.js
@@ -6,6 +6,7 @@ import { preLoaderAnim } from "../Animations";
 import logored from "../Images/logored.svg";
 import logobrain from "../Images/logobrain.svg";
 import logotext from "../Images/logotext.svg";
+import logokmutt from "../Images/logokmutt.png";
 
 const Preloader = () => {
   useEffect(() => {
@@ -20,7 +21,14 @@ const Preloader = () => {
   };
 
   const logobrainStyle = {
-    width: "60%",
+    width: "80%",
+    height: "auto",
+    // marginLeft: "auto",
+    // marginRight: "4rem",
+  };
+
+  const logokmuttStyle = {
+    width: "70%",
     height: "auto",
     // marginLeft: "auto",
     // marginRight: "4rem",
@@ -28,19 +36,22 @@ const Preloader = () => {
 
   return (
     <div className="preloader">
-      <div className="texts-container">
-        <span className="w-50 items-center">
-          <img src={logobrain} loading="lazy" style={logobrainStyle} />
+      <div className="flex items-center texts-container">
+        <span className="w-60 items-center pe-4" style={{ textAlign: "right" }}>
+          <img src={logokmutt} loading="lazy" style={logokmuttStyle} />
         </span>
         <span className="w-100 items-center">
-          <img src={logotext} loading="lazy" style={logotextStyle} />
+          <img src={logored} loading="lazy" style={logobrainStyle} />
         </span>
+        {/* <span className="w-100 items-center">
+          <img src={logotext} loading="lazy" style={logotextStyle} />
+        </span> */}
         {/* <span className="pe-4 align-middle">
           Neuroscience Center for Research and Innovation
         </span> */}
         {/* <span className="pe-3">for Research and Innovation</span> */}
         {/* <span className="pe-4">for Research and Innovation</span> */}
-        <span className="ps-5 align-middle">KMUTT</span>
+        {/* <span className="ps-5 align-middle">KMUTT</span> */}
       </div>
     </div>
   );
