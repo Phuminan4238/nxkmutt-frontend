e9259122f79e0dd2e148780daad5126430631bb7
Phuminan4238 <phuminan@live.com>
2024-05-13 14:32:51 +0700

    update contact social, contact lab url - 13 May 2023 (2)

--- src/Components/Contactsocial.jsx
+++ src/Components/Contactsocial.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { LanguageContext } from "./LanguageContext";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
   faYoutube,
+  faInstagram,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 
 const ContactInfo = () => {
-  const [socialMediaData, setSocialMediaData] = useState([]);
+  const [facebookData, setFacebookData] = useState(null);
+  const [youtubeData, setYoutubeData] = useState(null);
+  const [instagramData, setInstagramData] = useState(null);
   const [hasDataFetched, setHasDataFetched] = useState(false);
 
   useEffect(() => {
@@ -17,10 +20,18 @@ const ContactInfo = () => {
       const fetchData = async () => {
         try {
           const response = await axios.get(
-            "http://10.2.14.173/api/contacts?populate=admin_staff.uploadfiles.fileupload"
+            "http://10.2.14.173/api/contacts?populate=uploadfiles.fileupload"
           );
           const data = response.data.data;
-          setSocialMediaData(data);
+          if (data && data.length > 0) {
+            const facebookInfo = data.find((item) => item.id === 6);
+            const youtubeInfo = data.find((item) => item.id === 7);
+            const instagramInfo = data.find((item) => item.id === 8);
+
+            setFacebookData(facebookInfo ? facebookInfo.attributes : null);
+            setYoutubeData(youtubeInfo ? youtubeInfo.attributes : null);
+            setInstagramData(instagramInfo ? instagramInfo.attributes : null);
+          }
           setHasDataFetched(true);
         } catch (error) {
           console.log(error);
@@ -31,48 +42,69 @@ const ContactInfo = () => {
     }
   }, [hasDataFetched]);
 
-  const cards = [
-    {
-      id: 6,
-      title: "Facebook",
-      icon: faFacebook,
-      color: "#0370e6",
-    },
-    {
-      id: 7,
-      title: "YouTube",
-      icon: faYoutube,
-      color: "red",
-    },
-    {
-      id: 8,
-      title: "Twitter",
-      icon: faTwitter,
-      color: "#1DA1F2",
-    },
-  ];
-
   return (
     <div className="container m-0">
       <div className="row">
-        {cards.map((card, index) => {
-          const socialData = socialMediaData.find(
-            (item) => item.id === card.id
-          );
-          return (
-            <div className="col" key={index}>
-              <div className="image-container d-flex align-items-center">
-                <FontAwesomeIcon
-                  icon={card.icon}
-                  size="3x"
-                  style={{ color: card.color }}
-                />
-                <p className="ps-4 text-center align-middle">{card.title}</p>
-                {socialData && <p>{socialData.content_en}</p>}
-              </div>
-            </div>
-          );
-        })}
+        <div className="col-sm-12 col-md-3 mb-3">
+          {facebookData && (
+            <Link
+              to={facebookData.content_en[0]}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center m-0 text-black text-decoration-none"
+            >
+              <FontAwesomeIcon
+                icon={faFacebook}
+                size="2x"
+                className="pr-3"
+                style={{ color: "blue" }}
+              />
+              <p className={`font-medium m-0 xs:text-sm md:text-lg`}>
+                {facebookData.header_en}
+              </p>
+            </Link>
+          )}
+        </div>
+        <div className="col-sm-12 col-md-3 mb-3">
+          {youtubeData && (
+            <Link
+              to={youtubeData.content_en[0]}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center text-black text-decoration-none"
+            >
+              <FontAwesomeIcon
+                icon={faYoutube}
+                size="2x"
+                className="pr-3"
+                style={{ color: "red" }}
+              />
+              <p className={`font-medium m-0 xs:text-sm md:text-lg`}>
+                {youtubeData.header_en}
+              </p>
+            </Link>
+          )}
+        </div>
+        <div className="col-sm-12 col-md-3 mb-3">
+          {instagramData && (
+            <Link
+              to={instagramData.content_en[0]}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center text-black text-decoration-none"
+            >
+              <FontAwesomeIcon
+                icon={faTwitter}
+                size="2x"
+                className="pr-3"
+                style={{ color: "blue" }}
+              />
+              <p className={`font-medium m-0 xs:text-sm md:text-lg`}>
+                {instagramData.header_en}
+              </p>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
